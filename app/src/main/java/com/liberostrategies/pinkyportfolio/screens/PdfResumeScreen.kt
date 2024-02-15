b301a8b93c00cb2d54eb925aa6b3615a18526483
pink <liberostrategies@gmail.com>
2024-02-15 07:36:38 -0500

    Register app with Firebase. Add Objectives section.

--- app/src/main/java/com/liberostrategies/pinkyportfolio/screens/PdfResumeScreen.kt
+++ app/src/main/java/com/liberostrategies/pinkyportfolio/screens/PdfResumeScreen.kt
@@ -3,13 +3,26 @@ package com.liberostrategies.pinkyportfolio.screens
 import android.content.Context
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Button
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import co.touchlab.kermit.Logger
 import coil.compose.AsyncImage
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import com.liberostrategies.pinkyportfolio.domain.AndroidDownloader
 
 @Composable
@@ -35,6 +48,9 @@ fun PdfResumeScreen(context: Context) {
             )
         }
         item {
+            Objective()
+        }
+        item {
             Button(
                 onClick = {
                     val downloader = AndroidDownloader(context)
@@ -45,4 +61,62 @@ fun PdfResumeScreen(context: Context) {
             }
         }
     }
+}
+
+@Composable
+fun Objective() {
+    val db = Firebase.firestore
+    val resumeDoc = db.collection("resume")
+    val docSummary = resumeDoc.document("summary")
+    var whoami by remember { mutableStateOf("TBD") }
+    var description by remember { mutableStateOf("TBD") }
+    var github by remember { mutableStateOf("TBD") }
+    docSummary.get()
+        .addOnSuccessListener { document ->
+            if (document != null) {
+                Logger.d("ResumeTypeScreen") { "DocumentSnapshot data: ${document.data}" }
+                whoami = document.data?.get("whoami").toString()
+                description = document.data?.get("description").toString()
+                github = document.data?.get("github").toString()
+            } else {
+                Logger.d("ResumeTypeScreen") { "No such document" }
+            }
+        }
+        .addOnFailureListener { exception ->
+            Logger.d("ResumeTypeScreen") { "get failed with ${exception}" }
+        }
+
+    ElevatedCard(
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 6.dp
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+    ) {
+        Text(
+            text = whoami,
+            modifier = Modifier
+                .padding(start = 8.dp, end = 8.dp, top = 8.dp),
+            textAlign = TextAlign.Start,
+            fontWeight = FontWeight.Bold
+        )
+        Text (
+            text = description,
+            modifier = Modifier
+                .padding(start = 8.dp, end = 8.dp),
+            textAlign = TextAlign.Start,
+        )
+        Text(
+            text = github,
+            modifier = Modifier
+                .padding(start = 8.dp, end = 8.dp, bottom = 8.dp),
+            textAlign = TextAlign.Start,
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+fun Company() {
+
 }
