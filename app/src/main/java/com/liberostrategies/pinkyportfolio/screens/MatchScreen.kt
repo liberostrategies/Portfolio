cd9cc29ef133a02f18c359edff95d83625529e9e
pink <liberostrategies@gmail.com>
2024-02-25 00:30:44 -0500

    Use Checkboxes.

--- app/src/main/java/com/liberostrategies/pinkyportfolio/screens/MatchScreen.kt
+++ app/src/main/java/com/liberostrategies/pinkyportfolio/screens/MatchScreen.kt
@@ -1,16 +1,24 @@
 package com.liberostrategies.pinkyportfolio.screens
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.selection.toggleable
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Checkbox
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
@@ -152,14 +160,33 @@ fun Category(
             style = MaterialTheme.typography.bodyMedium,
             fontWeight = FontWeight.Bold
         )
-        jobQualifications.forEach {
-            Text(
-                text = it.qualification,
-                modifier = Modifier
-                    .padding(start = 10.dp),
-                textAlign = TextAlign.Start,
-                style = MaterialTheme.typography.bodyMedium,
-            )
+        jobQualifications.forEach { it ->
+            val (checkedState, onStateChange) = remember { mutableStateOf(true) }
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .height(56.dp)
+                    .toggleable(
+                        value = checkedState,
+                        onValueChange = { onStateChange(!checkedState) },
+                        role = Role.Checkbox
+                    )
+                    .padding(horizontal = 16.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Checkbox(
+                    checked = checkedState,
+                    onCheckedChange = null // null recommended for accessibility with screenreaders
+                )
+
+                Text(
+                    text = it.qualification,
+                    modifier = Modifier
+                        .padding(start = 10.dp),
+                    textAlign = TextAlign.Start,
+                    style = MaterialTheme.typography.bodyMedium,
+                )
+            }
         }
     }
 }
