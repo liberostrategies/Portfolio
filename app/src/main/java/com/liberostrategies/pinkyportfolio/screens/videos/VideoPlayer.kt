8a0f8f5dfe6defee44c5215b3e9cf023340afeb0
pink <liberostrategies@gmail.com>
2024-05-08 22:42:27 -0400

    Add framework for new VideosScreen route.

--- app/src/main/java/com/liberostrategies/pinkyportfolio/screens/videos/VideoPlayer.kt
+++ app/src/main/java/com/liberostrategies/pinkyportfolio/screens/videos/VideoPlayer.kt
@@ -1,14 +1,25 @@
 package com.liberostrategies.pinkyportfolio.screens.videos
 
+import android.app.Activity
+import android.content.Context
+import android.content.ContextWrapper
+import android.content.pm.ActivityInfo
 import android.view.ViewGroup
-import android.widget.FrameLayout
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
@@ -19,8 +30,29 @@ import androidx.media3.ui.PlayerView
 import co.touchlab.kermit.Logger
 
 @Composable
+fun Context.findActivity(): Activity? = when (this) {
+    is Activity       -> this
+    is ContextWrapper -> baseContext.findActivity()
+    else              -> null
+}
+
+@Composable
+fun Context.SetScreenOrientation(orientation: Int) {
+    val activity = this.findActivity() ?: return
+    activity.requestedOrientation = orientation
+    if (orientation == ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE) {
+        //hideSystemUi()
+        Logger.d("VideoPlayer") { "Orientation landscape" }
+    } else {
+        //showSystemUi()
+        Logger.d("VideoPlayer") { "Orientation portrait" }
+    }
+}
+
+@Composable
 fun VideoPlayer(
-    videoUri: String
+    videoUri: String,
+    onClose: () -> Unit
 ) {
 
     val context = LocalContext.current
@@ -30,6 +62,8 @@ fun VideoPlayer(
     val mediaSource = remember(videoUri) {
         MediaItem.fromUri(videoUri)
     }
+
+    //context.SetScreenOrientation(orientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE)
 
     LaunchedEffect(mediaSource) {
         exoPlayer.setMediaItem(mediaSource)
@@ -48,6 +82,8 @@ fun VideoPlayer(
                 if (isPlaying) {
                     // Active playback.
                     Logger.d("VideoPlayer") { "ExoPlayer is playing $videoUri" }
+                    //PlayVideo(videoUri)
+
                 } else {
                     // Not playing because playback is paused, ended, suppressed, or the player
                     // is buffering, stopped or failed. Check player.playWhenReady,
@@ -59,56 +95,82 @@ fun VideoPlayer(
         }
     )
 
-    AndroidView(
-        factory = { ctx ->
-            PlayerView(ctx).apply {
-                player = exoPlayer
-/*
-                setFullscreenButtonClickListener {
-                    //  Handle full screen callback based on dialog visibility
-                    if(!dialog.isShowing){
-                        this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
-                        dialog = object : Dialog(this, android.R.style.Theme_Black_NoTitleBar_Fullscreen){
-                            override fun onBackPressed() {
-                                StyledPlayerView.switchTargetView(player, fullScreenPlayerView, playerView)
-                                this@MainActivity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-
-                                // You can change the drawable of the image button.
-                                playerView.findViewById<ImageButton>(com.google.android.exoplayer2.ui.R.id.exo_fullscreen)
-                                    .setImageResource(R.drawable.ic_fullscreen_expand)
-
-                                super.onBackPressed()
-                            }
-                        }
-                        dialog?.addContentView(
-                            fullScreenPlayerView,
-                            ViewGroup.LayoutParams(
-                                ViewGroup.LayoutParams.MATCH_PARENT,
-                                ViewGroup.LayoutParams.MATCH_PARENT
-                            )
-                        )
-                        dialog?.show()
-                        StyledPlayerView.switchTargetView(player, playerView, fullScreenPlayerView)
-                    } else {
-                        StyledPlayerView.switchTargetView(player, fullScreenPlayerView, playerView)
-                        this@MainActivity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
-                        dialog?.dismiss()
-                    }
-                }
-
-                // Full screen
-                layoutParams =
-                    FrameLayout.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.MATCH_PARENT
-                    )
-
- */
-
-            }
-        },
+    Box(
         modifier = Modifier
-            .fillMaxWidth()
-            .height(200.dp)
-    )
+            .background(Color.Cyan.copy(alpha = 0.8f))
+            .fillMaxSize()
+//            .fillMaxWidth()
+    ) {
+        AndroidView(
+            modifier = Modifier
+                .fillMaxSize()
+//                .fillMaxWidth()
+//                .fillMaxSize(),
+                    ,
+            factory = { ctx ->
+                PlayerView(ctx).apply {
+                    player = exoPlayer
+
+
+                    // Full screen
+//                    layoutParams =
+////                        FrameLayout.LayoutParams(
+//                    ViewGroup.LayoutParams(
+//                        ViewGroup.LayoutParams.MATCH_PARENT,
+//                        ViewGroup.LayoutParams.MATCH_PARENT
+//                    )
+
+                    /*
+                    setFullscreenButtonClickListener {
+                        //  Handle full screen callback based on dialog visibility
+                        if(!dialog.isShowing){
+                            this.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                            dialog = object : Dialog(this, android.R.style.Theme_Black_NoTitleBar_Fullscreen){
+                                override fun onBackPressed() {
+                                    StyledPlayerView.switchTargetView(player, fullScreenPlayerView, playerView)
+                                    this@MainActivity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+
+                                    // You can change the drawable of the image button.
+                                    playerView.findViewById<ImageButton>(com.google.android.exoplayer2.ui.R.id.exo_fullscreen)
+                                        .setImageResource(R.drawable.ic_fullscreen_expand)
+
+                                    super.onBackPressed()
+                                }
+                            }
+                            dialog?.addContentView(
+                                fullScreenPlayerView,
+                                ViewGroup.LayoutParams(
+                                    ViewGroup.LayoutParams.MATCH_PARENT,
+                                    ViewGroup.LayoutParams.MATCH_PARENT
+                                )
+                            )
+                            dialog?.show()
+                            StyledPlayerView.switchTargetView(player, playerView, fullScreenPlayerView)
+                        } else {
+                            StyledPlayerView.switchTargetView(player, fullScreenPlayerView, playerView)
+                            this@MainActivity.requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+                            dialog?.dismiss()
+                        }
+                    }
+    */
+
+
+                }
+            },
+        )
+        IconButton(
+            onClick = { onClose() },
+            modifier = Modifier
+                .align(Alignment.TopEnd)
+                .padding(16.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Default.Close,
+                contentDescription = "Close",
+                tint = Color.White
+            )
+        }
+    }
 }
+
+
--- app/src/main/java/com/liberostrategies/pinkyportfolio/screens/videos/VideosScreen.kt
+++ app/src/main/java/com/liberostrategies/pinkyportfolio/screens/videos/VideosScreen.kt
@@ -1,30 +1,138 @@
 package com.liberostrategies.pinkyportfolio.screens.videos
 
+import android.annotation.SuppressLint
 import android.content.Context
+import android.content.pm.ActivityInfo
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+import com.liberostrategies.pinkyportfolio.Job
 
+const val videoUriUnitTests = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+const val videoUriUiTests = "https://www.learningcontainer.com/wp-content/uploads/2020/05/sample-mp4-file.mp4"
 @Composable
 fun VideosScreen(context: Context) {
-    val videoUriUnitTests = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
 
-    LazyColumn(
+    Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(color = Color.Black)
+            .padding(5.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
     ) {
-        item { VideoPlayer(videoUriUnitTests) }
-        item { VideoPlayer(videoUriUnitTests) }
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(15.dp),
+            modifier = Modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+        ) {
+            item {
+                VideoListItem(title = "JUnit Tests", videoUri = videoUriUnitTests)
+            }
+            item {
+                VideoListItem(title = "Lint Unit Test Coverage", videoUri = videoUriUiTests)
+            }
+            item {
+                VideoListItem(title = "MockK Tests", videoUri = videoUriUiTests)
+            }
+            item {
+                VideoListItem(title = "Fake Google Play Billing API Test", videoUri = videoUriUiTests)
+            }
+            item {
+                VideoListItem(title = "Espresso Test", videoUri = videoUriUiTests)
+            }
+            item {
+                VideoListItem(title = "Use Case Tests using Truth", videoUri = videoUriUiTests)
+            }
+            item {
+                VideoListItem(title = "Architecture Components Dependencies", videoUri = videoUriUiTests)
+            }        }
+
+        val isLandscape = false
+        //val context = LocalContext.current
+        val activity = context.findActivity()
+        val enterFullscreen = {
+            if (activity != null) {
+                activity.requestedOrientation =
+                    ActivityInfo.SCREEN_ORIENTATION_USER_LANDSCAPE
+            }
+        }
+        val exitFullscreen = {
+            if (activity != null) {
+                @SuppressLint("SourceLockedOrientationActivity")
+                // Will reset to SCREEN_ORIENTATION_USER later
+                activity.requestedOrientation =
+                    ActivityInfo.SCREEN_ORIENTATION_USER_PORTRAIT
+            }
+        }
+    }
+}
+
+@Composable
+fun VideoListItem(title: String, videoUri: String) {
+
+    var openVideo by remember { mutableStateOf(false) }
+
+    ElevatedCard(
+        colors = CardDefaults.cardColors(
+            //containerColor = Color.Red,
+        ),
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(width = 1.dp, color = Color.Black)
+            .padding(1.dp)
+            .clickable(true,
+                onClick = {
+                    openVideo = true
+                }
+            )
+    ) {
+        Text(
+            text = title,
+            color = Color.Black,
+            fontSize = 16.sp,
+            lineHeight = 400.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(5.dp),
+        )
     }
 
+    if (openVideo) {
+        Dialog (
+            onDismissRequest = { openVideo = false }
+        ) {
+            VideoPlayer(videoUri = videoUri, onClose = {
+                openVideo = false
+            })
+        }
+    }
 
-//    Box(modifier = Modifier
-//            .fillMaxSize()) {
-//        VideoPlayer(videoUriUnitTests)
-//    }
 }
